9af4fbdf9408aad0800ef9b44a6c06b6b0eded37
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-01-31 22:45:50 +0000

    SystemUI: Add Sync tile
    Change-Id: I0c0bb51e736a01661566f1118f955e32eba96063
    Signed-off-by: Fiqri Ardyansyah <fiqri191002@gmail.com>

--- packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
+++ packages/SystemUI/src/com/android/systemui/custom/CustomModule.kt
@@ -22,6 +22,7 @@ import com.android.systemui.qs.tiles.CellularTile
 import com.android.systemui.qs.tiles.DataSwitchTile
 import com.android.systemui.qs.tiles.WifiTile
 import com.android.systemui.qs.tiles.HeadsUpTile
+import com.android.systemui.qs.tiles.SyncTile
 import com.android.systemui.qs.tiles.AODTile
 import com.android.systemui.qs.tiles.UsbTetherTile
 import com.android.systemui.qs.tiles.PowerShareTile
@@ -62,6 +63,12 @@ interface CustomModule {
     @IntoMap
     @StringKey(HeadsUpTile.TILE_SPEC)
     fun bindHeadsUpTile(headsUpTile: HeadsUpTile): QSTileImpl<*>
+
+    /** Inject SyncTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SyncTile.TILE_SPEC)
+    fun bindSyncTile(syncTile: SyncTile): QSTileImpl<*>
 
     /** Inject AODTile into tileMap in QSModule */
     @Binds
